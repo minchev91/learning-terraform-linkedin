6e80c6a615a549816d79705231ac72de148f6fda
Mincho Minchev <aston.martin.mch@gmail.com>
2025-10-01 01:26:58 +0300

    again redoing the module

--- main.tf
+++ main.tf
@@ -49,10 +49,27 @@ resource "aws_instance" "blog" {
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  name            = "blog-alb"
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+
+  access_logs = {
+    bucket = "my-alb-logs"
+  }
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+  }
 
   target_groups = {
     ex-instance = {
@@ -63,14 +80,6 @@ module "alb" {
       target_id        = aws_instance.blog.id
     }
   }
-
-    http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
 
   tags = {
     Environment = "dev"
